05737c47db29c5907613642651067393bd49f445
masarom <romansanmaria@gmail.com>
2023-10-20 19:16:34 +0200

    add landing page

--- lib/main.dart
+++ lib/main.dart
@@ -11,56 +11,61 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color(0x38509bff)),
+          useMaterial3: true,
+          fontFamily: 'FuturaBT Medium Condensed'),
+      home: LandingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+// Start page
+class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Using Scaffold to make easier use of widgets
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // Using Stack instead of Column to have background image
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'lib/images/landing.png',
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Welcome message
+                // Adding padding to separate text and button
+                const Padding(
+                  // Adding padding only to bottom
+                  padding: EdgeInsets.only(bottom: 85.0),
+                  child: Text(
+                    'Welcome',
+                    style: TextStyle(fontSize: 48, color: Colors.white),
+                  ),
+                ),
+                // Start button
+                // Adding SizedBox widget to change button width
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print('clic!');
+                    },
+                    child: const Text(
+                      'Start',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
