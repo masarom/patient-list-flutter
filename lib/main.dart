16d1b21cadf75beeba018357ef2e6dfabc61b21a
masarom <romansanmaria@gmail.com>
2023-10-21 20:31:08 +0200

    add main page layout

--- lib/main.dart
+++ lib/main.dart
@@ -92,40 +92,113 @@ class MainPage extends StatelessWidget {
     final appBarTextColor = Theme.of(context).colorScheme.primary;
 
     return Scaffold(
-        // adding widgets to edit the AppBar styles (shadow, borders)
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(60.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFFEBEBEB),
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(30.0),
-                bottomRight: Radius.circular(30.0),
+      // adding widgets to edit the AppBar styles (shadow, borders)
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFFEBEBEB),
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.4),
+                offset: const Offset(0, 4),
+                blurRadius: 8.0,
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  offset: const Offset(0, 4),
-                  blurRadius: 8.0,
+            ],
+          ),
+          child: AppBar(
+            // adding a row to include different elements
+            title: Row(
+              // Making automatic space between elements
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Hi, Welcome', style: TextStyle(color: appBarTextColor)),
+                Text(formattedDate),
+              ],
+            ),
+            backgroundColor: const Color(0xFFEBEBEB),
+            // Delete the "go back" default arrow
+            automaticallyImplyLeading: false,
+          ),
+        ),
+      ),
+      // Content
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Main title
+            const Padding(
+              padding: EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                'Patient List',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            const Padding(
+                padding: EdgeInsets.only(bottom: 58.0),
+                child: Text('There are no patients in the list yet',
+                    style: TextStyle(fontSize: 20, color: Color(0x8b8b8bff)))),
+            // Search bar
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Full name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          contentPadding: const EdgeInsets.all(13.0),
+                          hintStyle: const TextStyle(
+                              fontSize: 19.0, color: Colors.black)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: appBarTextColor, shape: BoxShape.circle),
+                    child: IconButton(
+                      icon: const Icon(Icons.search, color: Colors.white),
+                      onPressed: () {
+                        // Lógica de búsqueda aquí
+                      },
+                    ),
+                  ),
                 ),
               ],
             ),
-            child: AppBar(
-              // adding a row to include different elements
-              title: Row(
-                // Making automatic space between elements
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Hi, Welcome', style: TextStyle(color: appBarTextColor)),
-                  Text(formattedDate),
-                ],
-              ),
-              backgroundColor: const Color(0xFFEBEBEB),
-              // Delete the "go back" default arrow
-              automaticallyImplyLeading: false,
-            ),
+          ],
+        ),
+      ),
+      // add new patient icon
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 32.0),
+        child: FloatingActionButton(
+          backgroundColor: appBarTextColor,
+          onPressed: () {
+            print('new patient');
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 50.0,
           ),
         ),
-        body: const Column(children: [Text('Main page')]));
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
   }
 }
